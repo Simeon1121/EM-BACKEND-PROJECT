cd15673bb03d471ffb7fb0708a714143f46e4ab0
Simeon Obinna <sohondah@yahoo.com>
2024-05-22 08:58:48 -0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv/config')
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5340;
+const port = process.env.PORT || 4000;
 const connect = require('./config/DB');
 const authRoute = require('./routes/authRoute');
 const userRoute = require("./routes/userRoute")
