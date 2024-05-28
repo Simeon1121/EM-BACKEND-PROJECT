9b2f5b88fecab6c8cf65533389a6533ccebc0696
Simeon Obinna <sohondah@yahoo.com>
2024-05-28 11:36:33 -0600

    update my wor

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,80 +28,84 @@ const getBioProfile = async (req, res) => {
 
 // follow a user
 
+// follow a user 14-4-2024
 const followUser = async (req, res) => {
   try {
     const { followersId } = req.params;
     const { userId } = req.user;
 
-    // finding userId and followersId from db.js
+    // finding userId and followersId from our Db
     const user = await USER.findById(userId);
     const follower = await USER.findById(followersId);
     if (!user || !follower) {
       res
         .status(404)
-        .json({ success: false, message: "User or follower not found" });
+        .json({ success: false, message: "user or follower not found" });
       return;
     }
 
     if (userId === followersId) {
       res
         .status(400)
-        .json({ success: false, message: "You cannot follow yourself" });
+        .json({ success: false, message: "You cannot follow yourself " });
       return;
     }
+
     if (!user.following.includes(followersId)) {
       user.following.push(followersId);
-      follower.followers.push(userId)
-
+      follower.followers.push(userId);
       await user.save();
       await follower.save();
 
       res
-        .status(200)
-        .json({ success: true, message: "User followed successfully", user });
+        .status(201)
+        .json({ success: true, message: "user followed successfully", user });
     } else {
-      res
-        .status(400)
-        .json({
-          success: false,
-          message: "You are already following this user",
-        });
+      res.status(400).json({
+        success: false,
+        message: "You are already following this user",
+      });
     }
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
-// unfollow user
 
-const unfollowUser = async (req, res)=>{
-    try {
-        const {followersId} = req.params;
-        const {userId} = req.user;
-
-        // finding userId and followersId from db.js
-        const user = await USER.findById(userId);
-        const follower = await USER.findById(followersId);
-        if (!user || !follower) {
-            res.status(404).json({success:false, message: "User or follower not found"});
-            return;
-        }
-        if(user.following.includes(followersId)){
-            user.following = user.following.filter(id =>id.toString() !== followersId);
-            follower.followers = follower.followers.filter(id.toString() !==userId);
-            await user.save();
-            await follower.save();
-            res.status(200).json({success:true,
-            message:"user unfollowed successfully", user})
-        }else{
-            res.status(400).json({success:false,
-            message:"You are not following this user"})
-
-        }
-
-    } catch (error){
-        res.status(500).json(error.message)
-
+// unfollow a user
+const unfollowUser = async (req, res) => {
+  const { followersId } = req.params;
+  const { userId } = req.user;
+  try {
+    // finding userId and followersId from our Db
+    const user = await USER.findById(userId);
+    const follower = await USER.findById(followersId);
+    if (!user || !follower) {
+      res
+        .status(404)
+        .json({ success: false, message: "user or follower not found" });
+      return;
     }
+
+    if (user.following.includes(followersId)) {
+      user.following = user.following.filter(
+        (id) => id.toString() !== followersId
+      );
+      follower.followers = follower.followers.filter(
+        (id) => id.toString() !== userId
+      );
+      await user.save();
+      await follower.save();
+      res
+        .status(200)
+        .json({ success: true, message: "user unfollowed successfully", user });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "You are not following this user" });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
 
 // getUserProfile
@@ -226,7 +230,7 @@ const searchUsers = async(req,res)=>{
       
     } catch (error) {
       console.error('Error updating profile:', error);
-      res.status(500).json({succes: false,
+      res.status(500).json({success: false,
       message: 'Internal server error', error: error.message});
       
     }
